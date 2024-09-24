9befbc18ac560155f31dbc4d4b653f0e7954117b
Asish <asish.konkimalla@gmail.com>
2024-09-24 15:49:27 +0530

    first commit

---
+++ Spring Boot Getting started/springinitalizr/src/main/java/com/skillsoft/springinitalizr/SpringinitalizrApplication.java
@@ -0,0 +1,13 @@
+package com.skillsoft.springinitalizr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringinitalizrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringinitalizrApplication.class, args);
+	}
+
+}
---
+++ Spring Boot Getting started/springinitalizr/src/test/java/com/skillsoft/springinitalizr/SpringinitalizrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillsoft.springinitalizr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringinitalizrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
